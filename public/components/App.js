514be5aee8dd2251cc334af49015c50a8c461e50
gmeben <gmeben@gmail.com>
2020-08-12 17:42:19 -0400

    feat: Add the App component file

---
+++ public/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div>
+            <header>
+                <h1>Tic-Tac-Toe in React</h1>
+            </header>
+        </div>
+    )
+}
