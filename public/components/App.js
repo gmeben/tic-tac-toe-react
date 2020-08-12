7f39fdc40b39cca9fe7bc02e9c75e4eaf1ddb9aa
gmeben <gmeben@gmail.com>
2020-08-12 18:56:49 -0400

    feat: Draw a blank tic-tac-toe board

--- public/components/App.js
+++ public/components/App.js
@@ -6,6 +6,70 @@ export default function App() {
             <header>
                 <h1>Tic-Tac-Toe in React</h1>
             </header>
+            <Game />
         </div>
+    )
+}
+
+function generateGrid(rows, columns, mapper) {
+    return Array(rows)
+        .fill()
+        .map(() => {
+            return Array(columns).fill().map(mapper)
+        }
+    )
+}
+
+const newTicTacToeGrid = () => {
+    return generateGrid(3, 3, () => {
+        return null
+    })
+}
+
+function Game() {
+    const grid = newTicTacToeGrid()
+    return (
+        <div>
+            <Grid suppliedGrid={grid} />
+        </div>
+    )
+}
+
+function Grid({ suppliedGrid }) {
+    return (
+        <div style={{
+            display: 'inline-block'
+            }}>
+            <h2>Grid</h2>
+            <div style={{ 
+                backgroundColor: `#444`,
+                display: `grid`,
+                gridTemplateRows: `repeat(${suppliedGrid.length}, 1fr )`,
+                gridTemplateColumns: `repeat(${suppliedGrid[0].length}, 1fr)`,
+                gridGap: 2
+            }}>
+                {suppliedGrid.map((row, rowIndex) => {
+                    return row.map((value, columnIndex) => {
+                        return (
+                            <Cell 
+                                key={`${columnIndex}-${rowIndex}`}
+                                value={value} 
+                            />
+                        )
+                    })
+                })
+            }
+            </div>
+        </div>
+    )
+}
+
+function Cell() {
+    return (
+        <div style={{
+            backgroundColor: `#fff`,
+            width: 100,
+            height: 100
+        }}></div>
     )
 }
